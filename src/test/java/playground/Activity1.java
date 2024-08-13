ff98e6fd56c603b989152687f644ae98af5463cf
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-08-12 21:31:56 -0400

    Adding Activity1

---
+++ src/test/java/playground/Activity1.java
@@ -0,0 +1,32 @@
+package playground;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Activity1 {
+    private static WebDriver driver;
+
+    @BeforeMethod
+    public void openFacebook() {
+        driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void facebookTitle() {
+       String applicationTitle = driver.getTitle();
+        Assert.assertEquals(applicationTitle, "Facebook - log in or sign up" );
+    }
+    @AfterMethod
+    public void quitBrowser(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/playground/MyFirstTest.java
@@ -0,0 +1,11 @@
+package playground;
+
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+
+    @Test
+    public void myFirstTestMethod(){
+        System.out.println("hello world");}
+}
